f22f1a320159a1f1afce151ac4a101d2b31c0932
unknown <Administrator@IT-QA-14.BankAlEtihad.Com>
2022-05-07 15:15:12 +0300

    some changes on put movies

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -139,15 +139,9 @@ router.post('/addMovie', (req, res) => {
 })
 
 
+// PUT ROUTES - Must fill all the data to not come null
 
-
-// PATCH ROUTES
-
-
-
-
-
-router.patch('/editMovie/:id', (req, res) => {
+router.put('/editAllMovieFields/:id', (req, res) => {
 
 
     let id = req.params.id
@@ -193,6 +187,92 @@ router.patch('/editMovie/:id', (req, res) => {
             res.send({ "title": title, "duration": duration, "gross": gross, "num_voted_users": num_voted_users, "cast_total_facebook_likes": cast_total_facebook_likes, "plot_keywords": plot_keywords, "imdb_link": imdb_link, "num_user_for_reviews": num_user_for_reviews, "language": language, "country": country, "content_rating": content_rating, "budget": budget, "title_year": title_year, "imdb_score": imdb_score, "aspect_ratio": aspect_ratio, "movie_facebook_likes": movie_facebook_likes, "actors": actors, "color": color, "dir_id": dir_id })
         });
 
+
+
+    console.log('request.body one', req.body)
+
+});
+
+
+
+// PUT ROUTES -  No need to fill all the data to update some fields
+
+
+
+
+
+router.put('/editMovie/:id', (req, res) => {
+
+
+    let id = req.params.id
+    let title;
+    let duration
+    let gross
+    let genres
+    let num_voted_users
+    let cast_total_facebook_likes
+    let plot_keywords
+    let imdb_link
+    let num_user_for_reviews
+    let language
+    let country
+    let content_rating
+    let budget
+    let title_year
+    let imdb_score
+    let aspect_ratio
+    let movie_facebook_likes
+    let actors
+    let color
+    let dir_id
+
+connection.query(`SELECT * FROM movies WHERE movie_id = ${id}` ,
+      function (err, result) {
+            if (err) {
+                console.log(err);
+
+            }
+     req.body.title ? title = req.body.title : title = result[0].title
+     req.body.duration ? duration = req.body.duration : duration = result[0].duration
+     req.body.gross ? gross = req.body.gross : gross = result[0].gross
+     req.body.genres ? genres = JSON.stringify(req.body.genres) : genres = result[0].genres
+     req.body.num_voted_users ? num_voted_users = req.body.num_voted_users : num_voted_users = result[0].num_voted_users
+     req.body.cast_total_facebook_likes ? cast_total_facebook_likes = req.body.cast_total_facebook_likes : cast_total_facebook_likes = result[0].cast_total_facebook_likes
+     req.body.plot_keywords ? plot_keywords = JSON.stringify(req.body.plot_keywords) : plot_keywords = result[0].plot_keywords
+     req.body.imdb_link ? imdb_link = req.body.imdb_link : imdb_link = result[0].imdb_link
+     req.body.num_user_for_reviews ? num_user_for_reviews = req.body.num_user_for_reviews : num_user_for_reviews = result[0].num_user_for_reviews
+     req.body.language ? language = req.body.language : language = result[0].language
+     req.body.country ? country = req.body.country : country = result[0].country
+     req.body.content_rating ? content_rating = req.body.content_rating : content_rating = result[0].content_rating
+     req.body.budget ? budget = req.body.budget : budget = result[0].budget
+     req.body.title_year ? title_year = req.body.title_year : title_year = result[0].title_year
+     req.body.imdb_score ? imdb_score = req.body.imdb_score : imdb_score = result[0].imdb_score
+     req.body.aspect_ratio ? aspect_ratio = req.body.aspect_ratio : aspect_ratio = result[0].aspect_ratio
+     req.body.movie_facebook_likes ? movie_facebook_likes = req.body.movie_facebook_likes : movie_facebook_likes = result[0].movie_facebook_likes
+     req.body.color ? color = req.body.color : color = result[0].color
+     req.body.dir_id ? dir_id = req.body.dir_id : dir_id = result[0].dir_id
+
+
+     
+ 
+    // let director = req.body.director;
+
+
+            connection.query("UPDATE movies SET \
+            title = ?, duration = ?, gross = ?, genres = ?, num_voted_users = ?, cast_total_facebook_likes = ?, plot_keywords = ?, imdb_link = ?, num_user_for_reviews = ?, language = ?, country = ?, content_rating = ?, budget = ?, title_year = ?, imdb_score = ?, aspect_ratio = ?, movie_facebook_likes = ?, director = (SELECT name FROM directors WHERE director_id = ? LIMIT 1), color = ?, dir_id = ? WHERE movie_id = ?" ,
+                [title, duration, gross, genres, num_voted_users, cast_total_facebook_likes, plot_keywords, imdb_link, num_user_for_reviews, language, country, content_rating, budget, title_year, imdb_score, aspect_ratio, movie_facebook_likes, dir_id, color, dir_id, id], function (err, result) {
+                    if (err) {
+                        console.log(err);
+        
+                    }
+                   
+                    res.send({ "title": title, "duration": duration, "gross": gross, "num_voted_users": num_voted_users, "cast_total_facebook_likes": cast_total_facebook_likes, "plot_keywords": plot_keywords, "imdb_link": imdb_link, "num_user_for_reviews": num_user_for_reviews, "language": language, "country": country, "content_rating": content_rating, "budget": budget, "title_year": title_year, "imdb_score": imdb_score, "aspect_ratio": aspect_ratio, "movie_facebook_likes": movie_facebook_likes, "actors": actors, "color": color, "dir_id": dir_id })
+                });
+        
+
+});
+
+   
 
 
     console.log('request.body one', req.body)
